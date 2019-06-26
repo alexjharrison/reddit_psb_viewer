ffba07f69ac8e6ddb538dbce23120ffeee227b31
Alex Harrison <AlexJHarrison@gmail.com>
2019-06-25 22:41:31 -0400

    landing page done

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <span>{{this.data}}</span>
+  <div>
+    <h1>Photoshop Battalias</h1>
+    <div id="app">
+      <post-thumb v-for="post in posts" :post="post" :key="post.id"/>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import PostThumb from "./components/PostThumb";
 export default {
+  components: {
+    PostThumb
+  },
   data() {
     return {
-      data: ""
+      posts: ""
     };
   },
   async mounted() {
@@ -15,11 +24,36 @@ export default {
     const { data } = await axios.post("/api", {
       url: "http://api.reddit.com/r/photoshopbattles/hot"
     });
-    this.data = data;
-    console.log(this.data);
+    this.posts = data.data.children
+      .map(({ data }) => data)
+      .filter(({ is_self }) => !is_self);
+    console.log(this.posts);
   }
 };
 </script>
 
 <style scoped>
+#app {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: flex-start;
+}
+#app * {
+  margin: 10px;
+  border: 2px solid teal;
+  padding: 10px;
+  border-radius: 5px;
+}
 </style>
+<style>
+body {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: capitalize;
+}
+.pointer {
+  cursor: pointer;
+}
+</style>
+
---
+++ src/src/components/PostThumb.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    id="post-thumb"
+    class="pointer"
+    @click="$emit('modal',{
+      title, postUrl,id, photoUrl
+  })"
+  >
+    <div class="padding">{{title}}</div>
+    <img :src="photoUrl" :alt="title">
+    <div>
+      <a :href="postUrl">Link</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+  computed: {
+    photoUrl() {
+      return this.post.url;
+    },
+    postUrl() {
+      return "https://old.reddit.com" + this.post.permalink;
+    },
+    title() {
+      return this.post.title.split("PsBattle: ")[1];
+    },
+    id() {
+      return this.post.id;
+    }
+  }
+};
+</script>
+
+<style scoped>
+img {
+  object-fit: contain;
+  max-width: 300px;
+}
+#post-thumb {
+  font-size: 20px;
+}
+.padding {
+  padding: 10px 0;
+}
+</style>
